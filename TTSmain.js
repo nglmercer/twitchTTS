b94e5ec445a5d1feb3761b0d3b65de55a81c9df2
melserngl <nglmercer@gmail.com>
2024-11-04 19:30:15 -0800

    map and parse data

--- TTSmain.js
+++ TTSmain.js
@@ -1,27 +1,4 @@
-// Función para obtener audio de la API
-var voiceSelect = document.createElement('select');
-Object.keys(VOICE_LIST).forEach(function(key) {
-    var option = document.createElement('option');
-    option.text = key;
-    option.value = VOICE_LIST[key];
-    voiceSelect.appendChild(option);
-});
-console.log('Voz seleccionada:', voiceSelect.value);
-voiceSelect.addEventListener('change', function() {
-    fetchAudio(voiceSelect.value);
-});
-// Función para insertar texto en el chatbox
-function insertText(txt) {
-    const txtbox = document.createElement('text');
-    txtbox.innerText = txt;
-    add(document.createElement('br'));
-    add(txtbox);
-    Array.from(chatbox.children)
-        .slice(maxMsgInChat, Infinity)
-        .forEach(e => e.remove());
-}
-let lastMessage = "";
-let newChannel = '';
+
 async function changeChannel() {
     newChannel = channelInput.value;
     window.location.hash = '#' + newChannel;
@@ -34,99 +11,70 @@ async function changeChannel() {
     );
 }
 
-// Función para manejar los mensajes entrantes
-let lastUsername = '';
-async function fetchAudio(txt, customVoice) {
-    const resp = await fetch(TTS_API_ENDPOINT + makeParameters({ voice: voiceSelect.value, text: txt }));
-    if (resp.status != 200) return console.error("bad Message");
-    const blob = URL.createObjectURL(await resp.blob());
-    audioqueue.enqueue(blob);
-    if (!isPlaying) kickstartPlayer();
-}
 async function onMessage(channel, tags, msg, self) {
-    if (self || tags.username == newChannel) return; // nunca es verdadero, pero mejor prevenir que curar
-
-    let voice;
-    let start_of_msg = msg.indexOf(' ');
-    if (start_of_msg >= 0) {
-        let tmpVoice = msg.slice(1, start_of_msg);
-        if (msg[0] == VOICE_PREFIX && VOICE_LIST_ALT.indexOf(tmpVoice) >= 0) {
-            voice = tmpVoice;
-            console.log('cambiado la voz a:', tmpVoice);
-            msg = msg.slice(start_of_msg);
-        }
-    }
-
-    if (CHANNEL_BLACKLIST.some(ch => tags.username == ch))
-        return console.log('ignorado el mensaje de,', tags.username);
-    const usernameElement = document.createElement('span');
-    usernameElement.classList.add('username');
-    usernameElement.innerText = tags.username;
-
-    const messageElement = document.createElement('span');
-    messageElement.classList.add('message');
-    messageElement.innerText = msg;
-
-    const txtbox = document.createElement('div');
-    txtbox.appendChild(usernameElement);
-    txtbox.appendChild(document.createTextNode(': '));
-    txtbox.appendChild(messageElement);
-    insertText(txtbox);
-
-    //*
-    if (!voice) {
-        const pronoun = tags.username in pronoun_DB ?
-            pronoun_DB[tags.username] :
-            await fetch(PRONOUN_API_ENDPOINT + tags.username)
-            .then(resp => resp.text())
-            .then(JSON.parse)
-            .then(pronouns => {
-                if (pronouns.length) {
-                    const pronoun = pronouns[0].pronoun_id;
-                    pronoun_DB[tags.username] = pronoun;
-                    return pronoun;
-                }
-            });
-        if (pronoun && FEM_PRONOUNS.some(g => g == pronoun)) voice = voiceSelect.value;
-    }
-    let filterWordsInput = document.getElementById('filter-words').value;
-    // Buscar todas las secuencias de palabras entre barras (/)
-    let filterWords = (filterWordsInput.match(/\/(.*?)\//g) || []).map(word => word.slice(1, -1));
-    // Eliminar estas secuencias del texto de entrada
-    let remainingWords = filterWordsInput.replace(/\/(.*?)\//g, '');
-    // Dividir el texto restante por espacios y añadir las palabras al array
-    filterWords = filterWords.concat(remainingWords.split(/\s/).filter(Boolean));
-    let lowerCaseText = msg && msg.toLowerCase();
-    for (let word of filterWords) {
-        if (word && lowerCaseText.includes(word.toLowerCase())) {
-            return;
-        }
-    }
-    message = msg;
-    if (message === lastMessage) {
-        return;
-    }
-    if (msg.length <= 3) {
-        console.log('filtrado');
-        return;
-    }
-    //*/
-    if (lastUsername !== tags.username) {
-        fetchAudio(`${tags.username} dice:  ${msg}`, voice);
-        lastUsername = tags.username;
-    } else {
-        fetchAudio(`  ${msg}`, voice);
-    }
-    lastMessage = msg;
+    console.log("onMessage", msg, tags, channel, self);
 }
-
-function insertText(txt) {
-    add(document.createElement('br'));
-    add(txt);
-    Array.from(chatbox.children)
-        .slice(maxMsgInChat, Infinity)
-        .forEach(e => e.remove());
-}
+const eventsarray = [
+    "emote",
+    "emotes",
+    "chat",
+    "ban",
+    "clear",
+    "color",
+    "commercial",
+    "deletemessage",
+    "host",
+    "unhost",
+    "cheer",
+    "bits",
+    "cheers",
+    "connected",
+    "disconnected",
+    "maxreconnect",
+    "reconnect",
+    "action",
+    "message",
+    "emotesets",
+    "whisper",
+    "mod",
+    "unmod",
+    "r9kbeta",
+    "r9kmode",
+    "r9kbetaoff",
+    "r9kmodeoff",
+    "subscribers",
+    "subscriber",
+    "subscribersoff",
+    "mods",
+    "vips",
+    "ban",
+    "clear",
+    "color",
+    "commercial",
+    "deletemessage",
+    "host",
+    "unhost",
+    "join",
+    "part",
+    "subs_on",
+    "subs_off",
+    "slow",
+    "slowmode",
+    "slowoff",
+    "sub",
+    "resub",
+    "subgift",
+    "anonsubgift",
+    "submysterygift",
+    "anonsubmysterygift",
+    "primepaidupgrade",
+    "giftpaidupgrade",
+    "anongiftpaidupgrade",
+    "raided",
+    "newanchor",
+    "raid",
+    "ritual",
+]
 // Función para inicializar la aplicación
 window.onload = async function() {
     audio = document.getElementById("audio");
@@ -134,15 +82,10 @@ window.onload = async function() {
     button = document.getElementById("channel-button");
     skip = document.getElementById("skip-button");
     channelInput = document.getElementById("channel");
-    isPlaying = false;
-    add = (DESCENDING ? chatbox.prepend : chatbox.append).bind(chatbox);
-    audioqueue = new Queue();
     button.onclick = changeChannel;
-    skip.onclick = skipAudio;
     document.addEventListener("keyup", ({ key }) => {
         if (key == "Enter") changeChannel();
     });
-    audio.onended = kickstartPlayer;
     client = tmi.client();
     await client.connect()
         .then(() => {
@@ -152,5 +95,102 @@ window.onload = async function() {
                 return changeChannel();
             }
         }).catch(e => console.error('no se pudo conectar a twitch:', e))
-    client.on('chat', onMessage);
+        eventsarray.forEach(event => {
+            client.on(event, (...args) => {
+                // Guardar el último evento en el localStorage
+                localStorage.setItem(event, JSON.stringify(args));
+                const lastevent = "lastevent" + event;
+                localStorage.setItem(lastevent, JSON.stringify(args));
+                localStorage.setItem("lastevent", event);
+        
+                console.log(event, args);
+                // Mapear todos los datos de args en un solo objeto con sus claves originales
+                // const argsObject = args.reduce((acc, arg) => {
+                //     if (typeof arg === 'object' && arg !== null) {
+                //         // Si es un objeto, agregamos cada propiedad con su clave original
+                //         Object.entries(arg).forEach(([key, value]) => {
+                //             if (acc[key] !== undefined) {
+                //                 // Si la clave ya existe, conviértelo en un array o añade el nuevo valor
+                //                 acc[key] = Array.isArray(acc[key]) ? [...acc[key], value] : [acc[key], value];
+                //             } else {
+                //                 // Si la clave no existe, simplemente la añadimos
+                //                 acc[key] = value;
+                //             }
+                //         });
+                //     } else {
+                //         // Si es un valor primitivo, lo agregamos sin reemplazar el valor anterior
+                //         if (acc[event] !== undefined) {
+                //             acc[event] = Array.isArray(acc[event]) ? [...acc[event], arg] : [acc[event], arg];
+                //         } else {
+                //             acc[event] = arg;
+                //         }
+                //     }
+                //     return acc;
+                // }, {});
+                
+                // console.log("argsObject:", argsObject);
+                
+        
+        
+                onMessage(...args);
+            });
+        });
+        
+             
+
 }
+function getlastevent() {
+    const lastevent = localStorage.getItem("lastevent");
+    if (lastevent) {
+        return lastevent;
+    }
+}
+function getallevents() {
+    eventsarray.forEach(event => {
+        const item = localStorage.getItem("lastevent" + event);
+        if (item) {
+            const parsedItem = JSON.parse(item);
+            console.log(event, parsedItem);
+            console.log(mapEvent(event, parsedItem));
+        }
+    });
+}
+
+function mapEvent(eventType, eventData) {
+    const baseData = (data, commentIndex = null) => ({
+        uniqueId: data.username || eventData[1],
+        nickname: data["display-name"] || eventData[1],
+        isMod: data.mod,
+        isSub: data.subscriber,
+        isVip: data.vip,
+        comment: commentIndex !== null ? eventData[commentIndex] : undefined || data["system-msg"],
+        data
+    });
+
+    switch (eventType) {
+        case "chat":
+            return baseData(eventData[1], 2);
+        case "cheer":
+            return { ...baseData(eventData[1], 2), bits: eventData[1].bits };
+        case "join":
+            return { uniqueId: eventData[1], nickname: eventData[1], isMod: !eventData[2], isSub: !eventData[2] };
+        case "sub":
+            return baseData(eventData[4]);
+        case "resub":
+            return { ...baseData(eventData[5]), nickname: eventData[3], uniqueId: eventData[3] };
+        case "subgift":
+            return { ...baseData(eventData[5]), nickname: eventData[3], uniqueId: eventData[3] };
+        case "submysterygift":
+            return baseData(eventData[4]);
+        case "primepaidupgrade":
+            return baseData(eventData[3]);
+        case "giftpaidupgrade":
+            return baseData(eventData[4]);
+        case "raided":
+            return { ...baseData(eventData[3]), nickname: eventData[1], uniqueId: eventData[1] };
+        default:
+            return eventData;
+    }
+}
+
+getallevents();
